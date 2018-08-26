45f3f4f9ba0a2591e06f6c40cbd45b4bad517ab5
sarra232 <41738408+sarra232@users.noreply.github.com>
2018-08-26 01:06:07 -0500

    Implementación de menu

--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -34,5 +36,6 @@ public class FoodsActivity extends AppCompatActivity {
 
         }
     }
+
 
 }
--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,6 +35,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(drink);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_clear:
+                return true;
+
+            case R.id.menu_out:
+                finish();
+
+            default: super.onOptionsItemSelected(item);
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
