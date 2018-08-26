4ca0465e3b8d4b183a3d2300b30c2aca86d82602
Administrador <anamariaagudeloisigmail.com>
2018-08-26 00:01:38 -0500

    Implrementación vista Bebidas

--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
@@ -1,13 +1,38 @@
 package co.ude.copumovil.gr03_20182.lab1;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FoodsActivity extends AppCompatActivity {
-
+    ImageView imageFood;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foods);
+        imageFood = (ImageView) findViewById(R.id.imageVew_food);
     }
+    public void onClickImage(View view) {
+        cargarImagen();
+    }
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicación"), 10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Uri path = data.getData();
+            imageFood.setImageURI(path);
+
+        }
+    }
+
 }
--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/MainActivity.java
@@ -3,10 +3,11 @@ package co.ude.copumovil.gr03_20182.lab1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import static co.ude.copumovil.gr03_20182.lab1.R.*;
 
 public class MainActivity extends AppCompatActivity {
     Button food;
@@ -15,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
 
-        food = (Button) findViewById(R.id.butttonFood);
+        food = (Button) findViewById(id.butttonFood);
         food.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(food);
             }
         });
-        drink = (Button) findViewById(R.id.buttonDrink);
+        drink = (Button) findViewById(id.buttonDrink);
         drink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,4 +35,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
