8a83a75fac8cfc766adc020083cd040bb2528d7f
sarra232 <41738408+sarra232@users.noreply.github.com>
2018-08-26 13:42:54 -0500

    SharePreferecens

--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/DrinksActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/DrinksActivity.java
@@ -1,21 +1,39 @@
 package co.ude.copumovil.gr03_20182.lab1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinksActivity extends AppCompatActivity {
+    EditText editText_named, editText_priced, editText_ingredientD;
+    TextView textView_Result;
+
     ImageView imageDrink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
         imageDrink= (ImageView) findViewById(R.id.imageVew_drink);
+        editText_named = (EditText) findViewById(R.id.ediTex_nameD);
+        editText_priced = (EditText) findViewById(R.id.ediText_priceD);
+        editText_ingredientD = (EditText) findViewById(R.id.ediText_ingredientsD);
+        textView_Result = (TextView) findViewById(R.id.textView_addsD);
+
+        loadPreferences();
     }
+
+
+
     public void onClickImageD(View view)  {
         cargarImagen();
     }
@@ -33,6 +51,67 @@ public class DrinksActivity extends AppCompatActivity {
             imageDrink.setImageURI(path);
 
         }
+    }
+
+    public void onClick(View view){
+
+        switch (view.getId()){
+
+            case R.id.button_saveD:
+                savePreferences();
+
+                break;
+        }
+
+    }
+
+    private void savePreferences() {
+        SharedPreferences preferences = getSharedPreferences("fileXML", Context.MODE_PRIVATE);
+        String name = editText_named.getText().toString();
+        String price = editText_priced.getText().toString();
+        String ingredient = editText_ingredientD.getText().toString();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Nombre : ", name);
+        editor.putString("Precio : ", price);
+        editor.putString("Los ingredientes son: ", ingredient);
+
+        textView_Result.setText("Nombre :"+name+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
+        editor.commit();
+
+    }
+    private void loadPreferences() {
+        SharedPreferences preferences = getSharedPreferences("fileXML", Context.MODE_PRIVATE);
+        String name = preferences.getString("Nombre :", "No hay información ingresada");
+        String price = preferences.getString("Precio :", "No hay información ingresada");
+        String ingredient = preferences.getString("Ingredientes :", "No hay información ingresada");
+
+        textView_Result.setText("Nombre "+name+"\nPrecio "+price+"\nIngredientes "+ingredient);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_clear:
+                editText_named.setText("");
+                editText_priced.setText("");
+                editText_ingredientD.setText("");
+                break;
+
+            case R.id.menu_out:
+                finish();
+
+            default: super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
@@ -1,6 +1,8 @@
 package co.ude.copumovil.gr03_20182.lab1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -8,16 +10,38 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class FoodsActivity extends AppCompatActivity {
     ImageView imageFood;
+
+    EditText editText_namef,editText_pricef, editText_ingredientf;
+    CheckBox checkBox_schedulerM, checkBox_schedulerA, checkBox_schedulerN;
+    RadioButton radioButton_type_1, radioButton_type_2;
+    TextView textView_Resultf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foods);
         imageFood = (ImageView) findViewById(R.id.imageVew_food);
+        editText_namef = (EditText) findViewById(R.id.ediTex_name);
+        checkBox_schedulerA = (CheckBox) findViewById(R.id.checkBox_afternoon);
+        checkBox_schedulerM = (CheckBox) findViewById(R.id.checkBox_morning);
+        checkBox_schedulerN = (CheckBox) findViewById(R.id.checkBox_nigth);
+        radioButton_type_1 = (RadioButton) findViewById(R.id.radioButton_starers);
+        radioButton_type_2 = (RadioButton) findViewById(R.id.radioButton_main_dish);
+        editText_pricef = (EditText) findViewById(R.id.ediText_price);
+        editText_ingredientf = (EditText) findViewById(R.id.ediText_ingredients);
+        textView_Resultf = (TextView) findViewById(R.id.textView_adds);
+
+        loadPreferences();
     }
+
     public void onClickImage(View view) {
         cargarImagen();
     }
@@ -37,5 +61,86 @@ public class FoodsActivity extends AppCompatActivity {
         }
     }
 
+
+    public void onClick(View view){
+
+        switch (view.getId()){
+            case R.id.button_saveF:
+                savePreferences();
+                break;
+        }
+
+    }
+    private void savePreferences() {
+        SharedPreferences preferences = getSharedPreferences("fileXML", Context.MODE_PRIVATE);
+
+        String name = editText_namef.getText().toString();
+        String price = editText_pricef.getText().toString();
+        String ingredient = editText_ingredientf.getText().toString();
+        String schedulerf = "";
+        if(checkBox_schedulerM.isChecked()){
+            schedulerf += "Mañana";
+        }else if (checkBox_schedulerA.isChecked()){
+            schedulerf += "Tarde";
+        }else if(checkBox_schedulerN.isChecked()){
+            schedulerf += "Noche";
+        }
+        String typef = "";
+        if(radioButton_type_1.isChecked()){
+            typef += "Entrada";
+        }else if(radioButton_type_2.isChecked()){
+            typef += "Plato fuerte";
+        }
+
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Nombre : ", name);
+        editor.putString("El horario seleccionado es: ", schedulerf);
+        editor.putString("Tipo: ", typef);
+        editor.putString("Precio : ", price);
+        editor.putString("Los ingredientes son: ", ingredient);
+
+        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"\nTipo: "+typef+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
+
+        editor.commit();
+
+    }
+    private void loadPreferences() {
+        SharedPreferences preferences = getSharedPreferences("fileXML", Context.MODE_PRIVATE);
+        String name = preferences.getString("Nombre :", "No hay información ingresada");
+        String schedulerf = preferences.getString("Horario: ", "No hay información seleccionada");
+        String typef = preferences.getString("Tipo : ", "No hay información seleccionada");
+        String price = preferences.getString("Precio :", "No hay información ingresada");
+        String ingredient = preferences.getString("Ingredientes :", "No hay información ingresada");
+
+        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"Tipo: "+typef+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.menu_clear:
+                editText_namef.setText("");
+                editText_pricef.setText("");
+                editText_ingredientf.setText("");
+                break;
+            case R.id.menu_out:
+                finish();
+
+            default: super.onOptionsItemSelected(item);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
