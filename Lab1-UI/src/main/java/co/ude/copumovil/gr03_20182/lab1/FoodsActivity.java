6f5e028ff60fb6fb2d67227f7fb558c0d73ca885
Administrador <anamariaagudeloisigmail.com>
2018-08-26 22:16:17 -0500

    Commit Final

--- Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
+++ Lab1-UI/src/main/java/co/ude/copumovil/gr03_20182/lab1/FoodsActivity.java
@@ -19,7 +19,7 @@ import android.widget.TextView;
 public class FoodsActivity extends AppCompatActivity {
     ImageView imageFood;
 
-    EditText editText_namef,editText_pricef, editText_ingredientf;
+    EditText editText_namef,editText_pricef, editText_ingredientf,editText_time;
     CheckBox checkBox_schedulerM, checkBox_schedulerA, checkBox_schedulerN;
     RadioButton radioButton_type_1, radioButton_type_2;
     TextView textView_Resultf;
@@ -35,6 +35,7 @@ public class FoodsActivity extends AppCompatActivity {
         checkBox_schedulerN = (CheckBox) findViewById(R.id.checkBox_nigth);
         radioButton_type_1 = (RadioButton) findViewById(R.id.radioButton_starers);
         radioButton_type_2 = (RadioButton) findViewById(R.id.radioButton_main_dish);
+        editText_time = (EditText) findViewById(R.id.time);
         editText_pricef = (EditText) findViewById(R.id.ediText_price);
         editText_ingredientf = (EditText) findViewById(R.id.ediText_ingredients);
         textView_Resultf = (TextView) findViewById(R.id.textView_adds);
@@ -77,6 +78,7 @@ public class FoodsActivity extends AppCompatActivity {
         String name = editText_namef.getText().toString();
         String price = editText_pricef.getText().toString();
         String ingredient = editText_ingredientf.getText().toString();
+        String time= editText_time.getText().toString();
         String schedulerf = "";
         if(checkBox_schedulerM.isChecked()){
             schedulerf += "Mañana";
@@ -96,11 +98,12 @@ public class FoodsActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("Nombre : ", name);
         editor.putString("El horario seleccionado es: ", schedulerf);
-        editor.putString("Tipo: ", typef);
+        editor.putString("Tipo: ", name);
+        editor.putString("Tiempo: ", typef);
         editor.putString("Precio : ", price);
         editor.putString("Los ingredientes son: ", ingredient);
 
-        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"\nTipo: "+typef+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
+        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"\nTipo: "+typef+"\nTiempo :"+time+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
 
         editor.commit();
 
@@ -110,10 +113,11 @@ public class FoodsActivity extends AppCompatActivity {
         String name = preferences.getString("Nombre :", "No hay información ingresada");
         String schedulerf = preferences.getString("Horario: ", "No hay información seleccionada");
         String typef = preferences.getString("Tipo : ", "No hay información seleccionada");
+        String time = preferences.getString("Tempo : ", "No hay información seleccionada");
         String price = preferences.getString("Precio :", "No hay información ingresada");
         String ingredient = preferences.getString("Ingredientes :", "No hay información ingresada");
 
-        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"Tipo: "+typef+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
+        textView_Resultf.setText("Nombre :"+name+"\nHorario: "+schedulerf+"Tipo: "+typef+"\nTiempo :"+time+"\nPrecio :"+price+"\nIngredientes :"+ingredient);
     }
 
 
@@ -130,6 +134,7 @@ public class FoodsActivity extends AppCompatActivity {
         switch (id){
             case R.id.menu_clear:
                 editText_namef.setText("");
+                editText_time.setText("");
                 editText_pricef.setText("");
                 editText_ingredientf.setText("");
                 break;
